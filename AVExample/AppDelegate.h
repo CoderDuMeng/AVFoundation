5a631f724e93c9af00e2f47b16bfbbd8235a68ef
dumengDu <dom@detu.com>
2016-06-28 12:44:44 +0800

    Initial Commit

---
+++ AVExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVExample
+//
+//  Created by detu on 16/6/28.
+//  Copyright © 2016年 demoDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
